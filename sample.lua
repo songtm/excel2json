ca5b91adb8fe49575b4456dce8977ac3b1be3d3e
songtm <565038419@qq.com>
2019-09-17 18:43:11 +0800

    todo 路径问题

---
+++ sample.lua
@@ -0,0 +1,38 @@
+local config={
+	init = {
+		coins = 1000,
+		exp = "",
+		golds = "",
+		character = "wizard",
+	},
+	buildings = {
+		barrack = {color = "b\"l\"ue\"",width = 200,height = 200,},
+		mine = {color = "yellow",width = 200,height = 100,},
+		gas = {color = "red",width = 100,height = 100,},
+		townhall = {color = "black",width = 200,height = 200,},
+	},
+	reqcoins = {
+		barrack = {100,500,1000,1500,2000,2500,},
+		mine = {100,500,1000,},
+		gas = {100,500,1000,},
+	},
+	shop = {
+		{name = "blade",price = 100,req_level = 1,category = "attack",desc = "Sword",},
+		{name = "dagger",price = 200,req_level = 2,category = "attack",desc = "Dagger",},
+		{name = "thorn_dagger",price = 300,req_level = 2,category = "attack",desc = "Thorn dagger",},
+		{name = "mail",price = 100,req_level = 2,category = "wear",desc = "Mail",},
+	},
+	inventory = {
+		{type = "blade",attrib = "",dru = true,},
+		{type = "thorn_dagger",attrib = {a = true,},dru = 150,},
+		{type = "mail",attrib = "",dru = 100,},
+		{type = "旬是什么",attrib = "",dru = "",},
+	},
+	friends = {
+		michael = {"tobby","cory","cain",},
+		tobby = {"michael","cory",},
+		tony = {"sarah","john",},
+		gump = {},
+	},
+}
+return config
