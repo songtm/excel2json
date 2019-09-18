36d12ef725d479edb575b02d2651c9376589da5f
songtm <565038419@qq.com>
2019-09-18 11:55:11 +0800

    *up

--- tool/doExport.bat
+++
@@ -1,3 +0,0 @@
-echo %1, %2
-cd /D "%~dp0"
-WSCRIPT.EXE  Excel2Json.js %1 %2
---
+++ tool/installer_compiler.bat
@@ -0,0 +1,4 @@
+
+cd /D "%~dp0"
+
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "install.ahk" /icon "icon.ico" /out ../install.exe
